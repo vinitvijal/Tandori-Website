c766badd3296b92e698d5a40696e7d5900b10177
Vinit Vijal <vineet.shanker@soesector17rohini.co.in>
2022-06-27 09:11:17 +0530

    new Data

--- prog.lenth and breath.py
+++
@@ -1,8 +0,0 @@
-length = float(input("Enter length of the rectangle :"))
-breadth =float(input("Enter breadth of the rectangle :"))
-area = length * breadth
-print ("Rectangle specifications")
-print ("Length=",length, end='')
-print ("breath = ",breadth)
-print ("area =",area)
-
